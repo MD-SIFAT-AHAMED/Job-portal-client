9d6fccfeaa3f46f63aee85d69e50f4d9d9fef5de
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-05-28 03:11:45 +0600

    User Sign in Funcitonalty added

--- src/Pages/Register/SignIn.jsx
+++ src/Pages/Register/SignIn.jsx
@@ -1,15 +1,44 @@
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { IoEyeOffOutline, IoEyeOutline } from "react-icons/io5";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import AuthContext from "../../Context/AuthContext";
+import toast from "react-hot-toast";
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const {signInWithEmail,signInWithGoogle}=use(AuthContext);
+  const navigate = useNavigate();
+
+  const handlerSubmit=(e)=>{
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    signInWithEmail(email,password)
+    .then(()=>{
+      toast.success("SignIn Successfully");
+      navigate('/');
+    })
+    .catch((err)=>{
+      toast.error(err.code);
+    })
+  }
+  const handlerGoogle = () => {
+    signInWithGoogle()
+      .then(() => {
+        toast.success("Login Successfully");
+        navigate('/');
+      })
+      .catch((err) => {
+        toast.error(err.code);
+      });
+  };
   return (
     <div className="bg-base-200">
       <div className="max-w-screen-2xl w-11/12 mx-auto place-items-center py-10">
         <div>
-          <form className="fieldset border-base-300 rounded-box w-xs md:w-md border md:text-base p-4 *:focus:outline-none *:border-none *:w-full">
+          <form onSubmit={handlerSubmit} className="fieldset border-base-300 rounded-box w-xs md:w-md border md:text-base p-4 *:focus:outline-none *:border-none *:w-full">
             <h3 className="text-2xl md:text-4xl text-center font-semibold">Sign In</h3>
             <label>Email</label>
             <input
@@ -31,7 +60,7 @@ const SignIn = () => {
               />
               <div
                 onClick={() => setShowPassword((prev) => !prev)}
-                className="absolute top-[13px] right-6"
+                className="absolute top-[13px] right-6 z-1"
               >
                 {showPassword ? (
                   <IoEyeOutline size={15} />
@@ -52,7 +81,7 @@ const SignIn = () => {
               value="Sign In"
             />
           </form>
-          <div className="flex justify-center items-center flex-col space-y-2">
+          <div onClick={handlerGoogle} className="flex justify-center items-center flex-col space-y-2">
             <p className="text-center">
               Don't have an account ?
               <Link to={"/register"}>
