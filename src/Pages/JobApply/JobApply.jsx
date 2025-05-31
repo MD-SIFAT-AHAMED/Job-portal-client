538e1416972b660f2d8fa74312d7257bbe3b1ae8
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-05-31 20:08:05 +0600

    job application information collected in the Database

--- src/Pages/JobApply/JobApply.jsx
+++ src/Pages/JobApply/JobApply.jsx
@@ -1,25 +1,76 @@
 import React from "react";
 import { useParams } from "react-router";
 import useAuth from "../../Hooks/useAuth";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const JobApply = () => {
+
   const { id: jobId } = useParams();
   const { user } = useAuth();
-
   console.log(user);
+
+  const handlerApplySubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const applyData = Object.fromEntries(formData.entries(formData));
+    console.log(applyData);
+    
+    axios.post('http://localhost:5000/application',applyData)
+    .then(res => 
+    {
+      if(res.data.insertedId)
+      {
+        toast.success("Apply Success");
+      }
+    }
+    )
+    .catch(err=>{
+      toast.error(err);
+    })
+  }
+
   return (
-    <div className="">
-      <h3 className="text-3xl">Apply Job for </h3>
-      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <label className="label">Title</label>
-        <input type="text" className="input" placeholder="My awesome page" />
+    <div className="max-w-screen-2xl w-11/12 mx-auto place-items-center my-10">
+      <h3 className="text-3xl my-5">Apply Job for </h3>
+      <form
+        onSubmit={handlerApplySubmit}
+        className="fieldset bg-base-200 border-base-300 rounded-box w-xs md:w-md border p-4 *:focus:outline-none *:text-black md:text-base *:border-none "
+      >
+        <label className="label">LinkedIn Link</label>
+        <input
+          type="url"
+          name="linkedIn"
+          required
+          className="input w-full"
+          placeholder="LinkedIn Profile Link"
+        />
 
-        <label className="label">Slug</label>
-        <input type="text" className="input" placeholder="my-awesome-page" />
+        <label className="label">Github Link</label>
+        <input
+          type="text"
+          name="github"
+          required
+          className="input w-full"
+          placeholder="Github Link"
+        />
 
-        <label className="label">Author</label>
-        <input type="text" className="input" placeholder="Name" />
-      </fieldset>
+        <label className="label">Resume Link</label>
+        <input
+          type="url"
+          name="resume"
+          required
+          className="input w-full"
+          placeholder="Resume Link"
+        />
+
+        <input
+          type="submit"
+          value="Apply"
+          className="btn !text-white btn-primary my-2 w-full"
+        />
+      </form>
     </div>
   );
 };
